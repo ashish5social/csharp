0af5fc9b8d4f8a7df09be9551a2e28d68579fd60
Ashish <kumar.ashish@salesforce.com>
2019-09-30 02:15:28 +0530

    Second

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
 // you can also use other imports, for example:
 // using System.Collections.Generic;
 
@@ -7,27 +11,35 @@
 
 class Solution
 {
+    public static void Main(String[] args)
+    {
+        IWebDriver webDriver = new ChromeDriver();
+        webDriver.Navigate().GoToUrl("http://bigbasket.com");
+        IWebElement searchBox = webDriver.FindElement(By.Id("input"));
+        searchBox.SendKeys("Potato");
+        System.Threading.Thread.Sleep(1000);
+        IWebElement searchBtn = webDriver.FindElement(By.XPath("//button[@qa='searchBtn']"));
+        searchBtn.Click();
+        //System.Threading.Thread.Sleep(1000);
+        //int[] A = { 2, 1, 3, 4, 6 };
+        //Solution s = new Solution();
+        //int answer = s.solution(A);
+        webDriver.Quit(); 
+    }
+
     public int solution(int[] A)
     {
-        Array.Sort(A); 
-        int minPosotivieTillyet = 1; 
-        foreach(int i in A)
+        Array.Sort(A);
+        int minPosotivieTillyet = 1;
+        foreach (int i in A)
         {
             if (i <= 0) continue;
             else if (i == minPosotivieTillyet)
             {
-                minPosotivieTillyet += 1; 
+                minPosotivieTillyet += 1;
             }
-
         }
 
-        return minPosotivieTillyet; 
-    }
-
-    public static void Main(String[] args)
-    {
-        int[] A = { 2, 1, 3, 4, 6 };
-        Solution s = new Solution();
-        int answer = s.solution(A); 
+        return minPosotivieTillyet;
     }
 }
